58a21443fd421a89182a606c8dfe48c450d6af65
repolevedavaj <jonas.bamberger@gmail.com>
2022-06-01 20:09:26 +0200

    added support for mvnd

--- src/main/java/io/projectenv/tools/ToolsIndex.java
+++ src/main/java/io/projectenv/tools/ToolsIndex.java
@@ -3,12 +3,14 @@ package io.projectenv.tools;
 import io.projectenv.core.commons.system.OperatingSystem;
 import org.immutables.gson.Gson;
 import org.immutables.value.Value;
+import org.immutables.value.Value.Style.ValidationMethod;
 
 import java.util.*;
 
 @Gson.TypeAdapters
 @Value.Immutable
 @Value.Modifiable
+@Value.Style(validationMethod = ValidationMethod.NONE)
 public interface ToolsIndex {
 
     SortedMap<String, SortedMap<String, SortedMap<OperatingSystem, String>>> getJdkVersions();
@@ -18,6 +20,8 @@ public interface ToolsIndex {
     SortedMap<String, String> getGradleVersions();
 
     SortedMap<String, String> getMavenVersions();
+
+    SortedMap<String, SortedMap<OperatingSystem, String>> getMvndVersions();
 
     SortedMap<String, SortedMap<OperatingSystem, String>> getNodeVersions();
 
---
+++ src/main/java/io/projectenv/tools/maven/MavenDaemonVersionsDatasource.java
@@ -0,0 +1,74 @@
+package io.projectenv.tools.maven;
+
+import io.projectenv.core.commons.process.ProcessOutput;
+import io.projectenv.core.commons.system.OperatingSystem;
+import io.projectenv.tools.ImmutableToolsIndex;
+import io.projectenv.tools.SortedCollections;
+import io.projectenv.tools.ToolsIndex;
+import io.projectenv.tools.ToolsIndexExtender;
+import io.projectenv.tools.jdk.github.GithubClient;
+import io.projectenv.tools.jdk.github.Release;
+
+import java.util.Comparator;
+import java.util.SortedMap;
+import java.util.regex.Pattern;
+
+public class MavenDaemonVersionsDatasource implements ToolsIndexExtender {
+
+    private static final Pattern RELEASE_TAG_PATTERN = Pattern.compile("^([\\d.]+)$");
+
+    // mvnd-0.7.1-darwin-amd64.zip
+    private static final Pattern RELEASE_ASSET_NAME_PATTERN = Pattern.compile("^mvnd-[\\d.]+-(\\w+)-amd64\\.zip$");
+
+    private final GithubClient githubClient;
+
+    public MavenDaemonVersionsDatasource(GithubClient githubClient) {
+        this.githubClient = githubClient;
+    }
+
+    @Override
+    public ToolsIndex extendToolsIndex(ToolsIndex currentToolsIndex) {
+        SortedMap<String, SortedMap<OperatingSystem, String>> mvndVersions = SortedCollections.createNaturallySortedMap(currentToolsIndex.getMvndVersions());
+
+        var releases = githubClient.getReleases("apache", "maven-mvnd")
+                .stream()
+                .sorted(Comparator.comparing(Release::getTagName))
+                .toList();
+
+        for (var release : releases) {
+            var releaseTagNameMatcher = RELEASE_TAG_PATTERN.matcher(release.getTagName());
+            if (!releaseTagNameMatcher.find()) {
+                ProcessOutput.writeInfoMessage("unexpected release tag name {0}", release.getTagName());
+                continue;
+            }
+
+            var version = releaseTagNameMatcher.group(1);
+
+            for (var releaseAsset : release.getAssets()) {
+                var releaseAssetNameMatcher = RELEASE_ASSET_NAME_PATTERN.matcher(releaseAsset.getName());
+                if (!releaseAssetNameMatcher.find()) {
+                    continue;
+                }
+
+                var operatingSystem = releaseAssetNameMatcher.group(1);
+
+                mvndVersions
+                        .computeIfAbsent(version, (key) -> SortedCollections.createNaturallySortedMap())
+                        .put(mapToOperatingSystem(operatingSystem), releaseAsset.getBrowserDownloadUrl());
+            }
+        }
+
+        return ImmutableToolsIndex.builder()
+                .from(currentToolsIndex)
+                .mvndVersions(mvndVersions)
+                .build();
+    }
+
+    private OperatingSystem mapToOperatingSystem(String operatingSystemName) {
+        return switch (operatingSystemName) {
+            case "darwin" -> OperatingSystem.MACOS;
+            default -> OperatingSystem.valueOf(operatingSystemName.toUpperCase());
+        };
+    }
+
+}
