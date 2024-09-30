4385441607b7034cc7172d9d358ec1fd29221850
repolevedavaj <jonas.bamberger@gmail.com>
2024-09-30 09:19:56 +0200

    Replace Semver comparison with manual comparison

--- src/main/java/io/projectenv/tools/SortedCollections.java
+++ src/main/java/io/projectenv/tools/SortedCollections.java
@@ -1,7 +1,5 @@
 package io.projectenv.tools;
 
-import org.semver4j.Semver;
-
 import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
@@ -98,7 +96,22 @@ public final class SortedCollections {
     }
 
     private static int compareVersions(String version1, String version2) {
-        return Semver.coerce(version1).compareTo(Semver.coerce(version2));
+        int comparisonResult = 0;
+
+        String[] version1Splits = version1.split("[.+]");
+        String[] version2Splits = version2.split("[.+]");
+        int maxLengthOfVersionSplits = Math.max(version1Splits.length, version2Splits.length);
+
+        for (int i = 0; i < maxLengthOfVersionSplits; i++) {
+            Integer v1 = i < version1Splits.length ? Integer.parseInt(version1Splits[i]) : 0;
+            Integer v2 = i < version2Splits.length ? Integer.parseInt(version2Splits[i]) : 0;
+            int compare = v1.compareTo(v2);
+            if (compare != 0) {
+                comparisonResult = compare;
+                break;
+            }
+        }
+        return comparisonResult;
     }
 
 }
